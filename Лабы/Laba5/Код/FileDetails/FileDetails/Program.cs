350c7d49b225e5b8967e10d00b0999d60b33593f
Nef0o0r <midmial2005@gmail.com>
2024-09-23 09:37:50 +0300

    Initial commit

---
+++ Лабы/Laba5/Код/FileDetails/FileDetails/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+class FileDetails
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int length;
+            string fileName;
+
+            // Вывод длины массива args
+            Console.WriteLine("Lengths of args: " + args.Length);
+
+            // Вывод всех аргументов
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            // Получение имени файла
+            fileName = args[0];
+            Console.WriteLine("Filename: " + fileName);
+
+            // Открываем файл и читаем его содержимое
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            // Получаем длину файла
+            length = (int)stream.Length;
+            char[] contents = new char[length];
+
+            // Читаем символы по одному и добавляем их в массив
+            for (int i = 0; i < length; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+
+            //Выводим символы в консоль
+            /*foreach (char c in contents)
+            {
+                Console.WriteLine(c);
+            }*/
+            // Закрываем потоки
+            reader.Close();
+            stream.Close();
+            Summarize(contents);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+    static void Summarize(char[] chars)
+    {
+        int lineCount = 1;  // Начинаем с 1, так как первая строка уже существует
+        int vowelCount = 0; // Счетчик гласных
+        int consonantCount = 0; // Счетчик согласных
+
+        foreach (char c in chars)
+        {
+            if ("AEIOUaeiouАУОИЭЫЁЕЮЯауоиеэыёеяю".IndexOf(c) != -1)
+            {
+                // Если символ гласный
+                vowelCount++;
+            }
+            else if (char.IsLetter(c))  // Если это буква, но не гласная, значит согласная
+            {
+                consonantCount++;
+            }
+
+            if (c == '\n')
+            {
+                lineCount++; // Увеличиваем количество строк
+            }
+        }
+
+        Console.WriteLine($"Гласные = {vowelCount}, согласные = {consonantCount}, строк = {lineCount}");
+    }
+
+}
+//cd C:\Users\Nef0o0r\OneDrive\Study\Второй курс\ООП C#\Лабы\Laba5\Код\FileDetails\FileDetails\bin\Debug>
+//FileDetails.exe text.txt
+//Код работает только через консоль!!!
---
+++ Лабы/Laba5/Код/Matrix/Matrix/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[2, 2];
+            int[,] arr2 = new int[2, 2];
+            int[,] result = new int[2, 2];
+            string line;
+
+            Console.WriteLine("Введите первую матрицу, каждый символ через Enter");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    line = Console.ReadLine();
+                    arr1[i,j] = int.Parse(line);
+                }
+            }
+
+
+            Console.WriteLine("Введите вторую матрицу, каждый символ через Enter");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    line = Console.ReadLine();
+                    arr2[i, j] = int.Parse(line);
+                }
+            }
+
+            Console.WriteLine("Первый массив:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr1[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Вторая массив:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(arr2[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+            MatrixMultiply multiplier = new MatrixMultiply();
+            multiplier.MatrixMultiplys(arr1, arr2, ref result);
+
+            Console.WriteLine("Матрица полученная в результате умножения:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+    internal class MatrixMultiply
+    {
+        public void MatrixMultiplys(int[,] arr1, int[,] arr2, ref int[,] result)
+        {   
+            for(int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    result[i,j] = 0;
+                    for (int k = 0; k < arr1.GetLength(0); k++)
+                        result[i,j] += arr1[i,k] * arr2[k,j];
+                }
+            }
+        }
+    }
+}
