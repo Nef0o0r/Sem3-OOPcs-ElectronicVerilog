b2984f796dede0c7c26318fa1d1f49237c1e1c8c
Nef0o0r <midmial2005@gmail.com>
2024-11-21 21:36:26 +0300

    lab11 OOP

---
+++ oop/lab11/code/OopLabs.Linq/OopLabs.Linq/Form1.Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace OopLabs.Linq
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ArtistsListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox AlbumsListBox;
+    }
+}
+
