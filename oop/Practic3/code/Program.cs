16d370ade335691ce23fbc3a9790c8d4484f442f
Николай Игоревич Овсейчик <niovseychik@stud.etu.ru>
2024-11-21 21:39:00 +0300

    3 практика - код без отчёта

---
+++ oop/Practic3/code/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pattern3;
+
+namespace Pattern3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Проверяем Singleton
+            Console.WriteLine("Testing Singleton:");
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+
+            Console.WriteLine("\nTesting RegularClass:");
+            for (int i = 0; i < 11; i++)
+            {
+                RegularClass instance = new RegularClass();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
